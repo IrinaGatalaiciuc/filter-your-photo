d7f27511ebb3b561bb4f17961d00980e9a2a7917
Irina <131261791+IrinaGatalaiciuc@users.noreply.github.com>
2023-10-20 14:20:48 +0300

    add design

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,50 +1,94 @@
 <template>
   <div class="main">
-    <div>
-      <div class="image">
-        <img v-if="isImageVisible" :class=[imgFilters] :style="imgStyles" src="../assets/nature-photo.jpg">
-        <p v-else>V will return soon</p>
+    <h1>Filter your image with US</h1>
+    <div class="container">
+      <div>
+        <div class="image">
+          <img
+            v-if="isImageVisible"
+            :class="[imgFilters]"
+            :style="imgStyles"
+            src="../assets/nature-photo.jpg"
+          />
+          <p v-else>Don't worry, your image will return soon!<br /><br />Press "Show"</p>
+        </div>
+        <div class="controls">
+          <button
+            @click="isImageVisible = !isImageVisible"
+            :class="isImageVisible ? ' ' : 'active'"
+          >
+            {{ isImageVisible ? "Hide" : "Show" }}
+          </button>
+        </div>
       </div>
-      <div class="controls">
-        <h1>Visions of V</h1>
+      <div>
         <h2>Filters</h2>
         <div class="btn-group flex">
-          <button type="button" @click="imgFilters.sepia = !imgFilters.sepia" :class="imgFilters.sepia ? 'active' : ' '">
+          <button
+            type="button"
+            @click="imgFilters.sepia = !imgFilters.sepia"
+            :class="imgFilters.sepia ? 'active' : ' '"
+          >
             Sepia
           </button>
-          <button type="button" @click="imgFilters.border = !imgFilters.border"
-            :class="imgFilters.border ? 'active' : ' '">
+          <button
+            type="button"
+            @click="imgFilters.grayscale = !imgFilters.grayscale"
+            :class="imgFilters.grayscale ? 'active' : ' '"
+          >
+            Grayscale
+          </button>
+          <button
+            type="button"
+            @click="imgFilters.border = !imgFilters.border"
+            :class="imgFilters.border ? 'active' : ' '"
+          >
             Border
           </button>
-          <button type="button" @click="imgFilters.shadow = !imgFilters.shadow"
-            :class="imgFilters.shadow ? 'active' : ' '">
+          <button
+            type="button"
+            @click="imgFilters.shadow = !imgFilters.shadow"
+            :class="imgFilters.shadow ? 'active' : ' '"
+          >
             Shadow
           </button>
         </div>
         <h2>Choose Size</h2>
-        <div class="btn-group">
+        <div class="for-size">
           <label>
-            Width: {{ imgSizes.currentWidth }}
-            <input type="range" :value="imgSizes.currentWidth" @input="imgSizes.currentWidth = $event.target.value"
-              :min="imgSizes.minWidth" :max="imgSizes.maxWidth">
+            Width: {{ imgSizes.currentWidth }} px
+            <input
+              type="range"
+              :value="imgSizes.currentWidth"
+              @input="imgSizes.currentWidth = $event.target.value"
+              :min="imgSizes.minWidth"
+              :max="imgSizes.maxWidth"
+            />
           </label>
           <label>
-            Height: {{ imgSizes.currentHeight }}
-            <input type="range" :value="imgSizes.currentHeight" @input="imgSizes.currentHeight = $event.target.value"
-              :min="imgSizes.minHeight" :max="imgSizes.maxHeight">
+            Height: {{ imgSizes.currentHeight }} px
+            <input
+              type="range"
+              :value="imgSizes.currentHeight"
+              @input="imgSizes.currentHeight = $event.target.value"
+              :min="imgSizes.minHeight"
+              :max="imgSizes.maxHeight"
+            />
           </label>
         </div>
         <h2>Rotate</h2>
-        <div class="btn-group">
+        <div class="rotate">
           <label>
             Angle: {{ rotate.value }}
-            <input type="range" :value="rotate.value" @input="rotate.value = $event.target.value" :min="rotate.min"
-              :max="rotate.max">
+            <input
+              type="range"
+              :value="rotate.value"
+              @input="rotate.value = $event.target.value"
+              :min="rotate.min"
+              :max="rotate.max"
+            />
           </label>
         </div>
-        <button @click="isImageVisible = !isImageVisible">
-          {{ isImageVisible ? ' Show ' : 'Hide' }}
-        </button>
       </div>
     </div>
   </div>
@@ -52,14 +96,15 @@
 
 <script>
 export default {
-  name: 'MyComponent',
+  name: "MyComponent",
   data() {
     return {
       isImageVisible: true,
       imgFilters: {
         sepia: false,
         border: false,
-        shadow: false
+        shadow: false,
+        grayscale: false,
       },
       imgSizes: {
         maxWidth: 680,
@@ -67,66 +112,25 @@ export default {
         minHeight: 300,
         minWidth: 400,
         currentWidth: 680,
-        currentHeight: 480
+        currentHeight: 480,
       },
       rotate: {
         min: 0,
         max: 360,
-        value: 0
-      }
-    }
+        value: 0,
+      },
+    };
   },
   computed: {
     imgStyles() {
       return {
         width: `${this.imgSizes.currentWidth}px`,
         height: `${this.imgSizes.currentHeight}px`,
-        transform: `rotate(${this.rotate.value}deg)`
-      }
-    }
-  }
-}
+        transform: `rotate(${this.rotate.value}deg)`,
+      };
+    },
+  },
+};
 </script>
 
-<style>
-.image {
-  height: 480px;
-  width: 680px;
-  background: #808080;
-}
-
-img {
-  height: 100%;
-  width: 100%;
-  object-fit: cover;
-  transition: 0.2s ease;
-
-  &.sepia {
-    filter: sepia(100%);
-  }
-
-  &.border {
-    border: 5px dashed black;
-  }
-
-  &.shadow {
-    box-shadow: 7px 7px 10px 0 grey;
-  }
-}
-
-button {
-  margin-right: 10px;
-
-  &.active {
-    background-color: aquamarine;
-  }
-}
-
-h2 {
-  margin-bottom: 10px;
-}
-
-.btn-group {
-  margin-bottom: 20px;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "./styleComponent.css"
 
 createApp(App).mount('#app')
